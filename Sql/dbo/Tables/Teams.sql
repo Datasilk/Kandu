2625ce787bb5f7a15c16ac2917a48eae803cbd28
Mark Entingh <entingh@gmail.com>
2017-09-21 14:32:59 -0400

    initial project setup

---
+++ Sql/dbo/Tables/Teams.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Teams]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ Sql/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[userId] INT NOT NULL PRIMARY KEY, 
+    [email] NVARCHAR(50) NOT NULL, 
+    [password] NVARCHAR(255) NOT NULL DEFAULT '', 
+    [photo] BIT NOT NULL DEFAULT 0, 
+    [datecreated] DATETIME NOT NULL DEFAULT GETDATE()
+)
