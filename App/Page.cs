b25fb757a351fa91d3d7a603b1af21f402a79ede
Mark Entingh <entingh@gmail.com>
2017-12-28 18:31:10 -0800

    fixed merge, can now use Datasilk Core

--- App/Page.cs
+++ App/Page.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace Kandu
+﻿namespace Kandu
 {
     public class Page : Datasilk.Page
     {
-<<<<<<< HEAD
         public Page(global::Core DatasilkCore) : base(DatasilkCore) {
             title = "Kandu";
             description = "You can do everything you ever wanted";
