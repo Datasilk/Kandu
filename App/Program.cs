34e5310ae1a7a7e14b50d530ff6e0e64c3f71847
Mark Entingh <entingh@gmail.com>
2022-05-13 20:26:21 -0500

    updated program.cs to use default .NET 6.0 class template

--- App/Program.cs
+++ App/Program.cs
@@ -1,32 +1,25 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 
 namespace Kandu
 {
     public class Program
     {
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                //.UseIISIntegration()
-                .UseKestrel(
-                    options =>
-                    {
-                        options.Limits.MaxRequestBodySize = null;
-                    }
-                )
-                .UseStartup<Startup>();
-            });
-
-
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
